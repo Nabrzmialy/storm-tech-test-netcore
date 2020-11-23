5bb270130cd1b9af485ee92c59690309d72dcbee
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-11-23 23:31:29 +0100

    Add missing unit tests for factory - sort by importance.

---
+++ Todo.Tests/TodoListDetailViewmodelFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Todo.Data.Entities;
+using Todo.EntityModelMappers.TodoItems;
+using Todo.EntityModelMappers.TodoLists;
+using Todo.Models.TodoItems;
+using Xunit;
+
+namespace Todo.Tests
+{
+    public class TodoListDetailViewmodelFactoryTests
+    {
+        private readonly TodoList srcTodoItem;
+
+        public TodoListDetailViewmodelFactoryTests()
+        {
+            var todoList = new TestTodoListBuilder(new IdentityUser("alice@example.com"), "shopping")
+                    .WithItem("bread", Importance.Medium)
+                    .WithItem("milk", Importance.Low)
+                    .WithItem("egg", Importance.High)
+                    .Build();
+
+            srcTodoItem = todoList;
+        }
+
+        [Fact]
+        public void Create_TodoListMappedByImportance()
+        {
+            var viewmodel = TodoListDetailViewmodelFactory.Create(srcTodoItem);
+            var items = viewmodel.Items.ToList();
+            Assert.Equal(Importance.High, items[0].Importance);
+            Assert.Equal(Importance.Medium, items[1].Importance);
+            Assert.Equal(Importance.Low, items[2].Importance);
+        }
+    }
+}
